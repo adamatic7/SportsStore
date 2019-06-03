dddfd4bca1e9fb1ec8f3f0de4004cb0e66132f6d
Adam Miranda <adamballimiranda@mail.weber.edu>
2019-06-02 22:23:43 -0600

    Added Default view.

---
+++ SportsStore/Components/NavigationMenuViewComponents.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
